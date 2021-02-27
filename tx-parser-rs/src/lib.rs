447f73d0200558b938af6588571dbbd03003dc53
Tahar <tntmeijs@gmail.com>
2021-02-27 11:13:48 +0100

    Processing function returns the file's size

--- tx-parser-rs/src/lib.rs
+++ tx-parser-rs/src/lib.rs
@@ -1,16 +1,6 @@
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-extern {
-    pub fn alert(s: &str);
-}
-
-#[wasm_bindgen]
-pub fn greet(name: &str) {
-    alert(&format!("Hello, {}!", name));
-}
-
-#[wasm_bindgen]
-pub fn process(data: &[u8]) -> i32 {
-    return -1;
+pub fn process(data: &[u8]) -> usize {
+    return data.len();
 }
