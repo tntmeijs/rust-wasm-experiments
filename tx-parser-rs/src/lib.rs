4b6075dfd6c4d4e46a779ca546bf7917cccc1e81
Tahar <tntmeijs@gmail.com>
2021-02-27 14:32:00 +0100

    Transaction structure

--- tx-parser-rs/src/lib.rs
+++ tx-parser-rs/src/lib.rs
@@ -1,5 +1,25 @@
 use wasm_bindgen::prelude::wasm_bindgen;
 
+/// Represents a single transaction
+struct Transaction {
+    index: u32,
+    name: String,
+    min_price: f32,
+    max_price: f32
+}
+
+impl Transaction {
+    /// Create a new transaction
+    fn new(index: u32, name: &str, min_price: f32, max_price: f32) -> Transaction {
+        Transaction {
+            index: index,
+            name: name.to_owned(),
+            min_price: min_price,
+            max_price: max_price
+        }
+    }
+}
+
 /// Process a CSV file with transaction data into usable information
 /// For the sake of simplicity, all files are assumed to use standard ASCII encoding
 #[wasm_bindgen]
@@ -34,16 +54,32 @@ pub fn process(data: &[u8]) -> String {
         line_index += 1;
     }
 
-    return data_for_js.join("\n");
+    // TODO: Parse transactions list
+
+    "".to_owned()
 }
 
-pub fn parse_line(line: &str) -> Result<String, &str> {
+fn parse_line(line: &str) -> Result<Transaction, &str> {
     // A line is formatted like so: <index>, <name>, <min_price>, <max_price>
     let parts = line.split(", ").collect::<Vec<&str>>();
-    
+
     if parts.len() != 4 {
         return Err("Row does not contain the expected number of entries");
     }
 
-    Ok(line.to_owned())
+    // Extract individual items from the parsed row
+    let index       = parts[0].parse::<u32>();
+    let name        = parts[1];
+    let min_price   = parts[2].parse::<f32>();
+    let max_price   = parts[3].parse::<f32>();
+
+    if index.is_err() {
+        return Err("Unable to parse index");
+    } else if min_price.is_err() {
+        return Err("Unable to parse min_price");
+    } else if max_price.is_err() {
+        return Err("Unable to parse max_price");
+    }
+    
+    Ok(Transaction::new(index.unwrap(), name, min_price.unwrap(), max_price.unwrap()))
 }
