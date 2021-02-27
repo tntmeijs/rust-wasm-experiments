07f084984c095412f4e85560a1879a0a8591f52c
Tahar <tntmeijs@gmail.com>
2021-02-27 16:36:52 +0100

    Timer to track processing time

--- demo/index.js
+++ demo/index.js
@@ -7,12 +7,18 @@ const onUploadFile = event => {
 };
 
 const parseFileWasm = blob => {
+    console.info("Starting processing now...");
+    const t0 = performance.now();
+    
     import("tx-parser-rs").then(txParser => {
         const reader = new FileReader();
         reader.readAsArrayBuffer(blob);
         reader.onload = () => {
             const bytes = new Uint8Array(reader.result);
-            console.info(JSON.parse(txParser.process(bytes)));
+            console.info(txParser.process(bytes));
+    
+            const t1 = performance.now();
+            console.info(`WebAssembly (Rust) processing time: ${t1 - t0} milliseconds`);
         };
     });
 }
