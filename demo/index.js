ac7dee7193fc54812fc8bf0286ec878a26c1b574
Tahar <tntmeijs@gmail.com>
2021-02-27 07:38:21 +0100

    File upload button and callback

--- demo/index.js
+++ demo/index.js
@@ -1,3 +1,15 @@
-import("tx-parser-rs").then(txParser => {
-    txParser.greet("WebAssembly")
-});
+const onUploadFile = event => {
+    const files = event.target.files;
+
+    for (let i = 0; i < files.length; ++i) {
+        parseFileWasm(files[i]);
+    }
+};
+
+const parseFileWasm = file => {
+    import("tx-parser-rs").then(txParser => {
+        txParser.greet(file.name)
+    });
+}
+
+document.getElementById("file-upload").addEventListener("change", onUploadFile);
